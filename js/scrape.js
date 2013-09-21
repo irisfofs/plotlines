98a0c76478b917d37c16df692249c6cc085ab944
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-21 19:09:47 -0400

    Scraper but wikitext

---
+++ js/scrape.js
@@ -0,0 +1,10 @@
+var headings = ['Plot', 'Synopsis', 'Plot summary', 'Summary'];
+
+var base = 'http://en.wikipedia.org/wiki/';
+
+function build_url(title) {
+	return base + encodeURIComponent(title);
+}
+function get_html(title) {
+	$.ajax())
+}
