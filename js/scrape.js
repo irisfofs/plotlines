7692d7a325b08e866c10a349c3dd4e2913b6c594
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-21 19:10:52 -0400

    More scraper but still wikitext

--- js/scrape.js
+++ js/scrape.js
@@ -1,10 +1,20 @@
 var headings = ['Plot', 'Synopsis', 'Plot summary', 'Summary'];
 
-var base = 'http://en.wikipedia.org/wiki/';
+//var base = 'http://en.wikipedia.org/wiki/';
+var base = 'http://en.wikipedia.org/w/api.php?action=query&export&format=json&callback=scrape';
 
 function build_url(title) {
-	return base + encodeURIComponent(title);
+	return base + '&titles=' + encodeURIComponent(title);
 }
 function get_html(title) {
-	$.ajax())
+	$.ajax({
+		'dataType': 'jsonp json',
+		'url': build_url(title),
+		'jsonpCallback': 'scrape'
+	});
 }
+
+function scrape(article) {
+	console.log(article.query.export['*']);
+}
+get_html('Andrew');
