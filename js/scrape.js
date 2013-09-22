5d3a9bdce3c5f16a995591ab5c8eeb05bee75a55
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-22 00:43:27 -0400

    Scraping away...

--- js/scrape.js
+++ js/scrape.js
@@ -6,23 +6,23 @@ var base = 'http://en.wikipedia.org/wiki/';
 function build_url(title) {
 	return base + encodeURIComponent(title);
 }
-function get_html(title) {
-	$.ajax({
+function get_xml(title) {
+	return $.ajax({
 		'dataType': 'xml',
 		'url': 'server/wp.php',
-		'success': scrape,
+		'async': false,
 		'data': {
 			'url': build_url(title)
 		}
-	});
+	}).responseXML;
 }
 
-function scrape(doc, status, xhr) {
-	return window.doc = doc2text(doc);
-}
-function doc2text(doc) {
-	return $(doc).find('#Plot_summary').parent().next('div').nextUntil('h2').toArray().map(function (v) {
+function scrape_wikipedia(title) {
+	var h2s = headings.map(function(v) { return 'h2 span:contains(' + v + ')' }).join(', ');
+	var doc = get_xml(title);
+
+	return $(doc).find(h2s).parent().next('div').nextUntil('h2').toArray().map(function (v) {
 	    return v.textContent;
 	}).join('\n');
 }
-get_html('To Kill a Mockingbird');
+
