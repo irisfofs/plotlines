92a05a2a7dd3410f1cf2d46d1132bfa9866d151e
Ophir Lifshitz <hangfromthefloor@gmail.com>
2013-09-22 03:15:32 -0400

    Small prep

--- js/scrape.js
+++ js/scrape.js
@@ -2,7 +2,6 @@ var headings = ['Plot', 'Synopsis', 'Plot summary', 'Summary'];
 var h2s = headings.map(function(v) { return 'h2 span:contains("' + v + '")' }).join(', ');
 
 var wikipedia_base = 'http://en.wikipedia.org/wiki/';
-// var base = 'http://en.wikipedia.org/w/api.php?action=query&export&format=json&callback=scrape';
 
 function build_wikipedia_url(title) {
 	return wikipedia_base + encodeURIComponent(title);
@@ -22,6 +21,7 @@ function scrape_wikipedia(title) {
 	var doc = get_xml(title);
 	window.doc = doc;
 
+	// jQuery can be neat and stuff
 	return $(doc).find(h2s).parent().nextUntil('h2').toArray().map(function (v) {
 	    return v.textContent;
 	}).join('\n');
