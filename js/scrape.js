f9c00b3c667889fc04e5d4ae860121135c709d7e
Xiagu <xiagu@xiagu.co>
2013-09-21 23:31:55 -0400

    works maybe

--- js/scrape.js
+++ js/scrape.js
@@ -1,20 +1,33 @@
 var headings = ['Plot', 'Synopsis', 'Plot summary', 'Summary'];
 
-//var base = 'http://en.wikipedia.org/wiki/';
-var base = 'http://en.wikipedia.org/w/api.php?action=query&export&format=json&callback=scrape';
+var base = 'http://en.wikipedia.org/wiki/';
+// var base = 'http://en.wikipedia.org/w/api.php?action=query&export&format=json&callback=scrape';
 
 function build_url(title) {
-	return base + '&titles=' + encodeURIComponent(title);
+	return base + encodeURIComponent(title);
 }
 function get_html(title) {
 	$.ajax({
-		'dataType': 'jsonp json',
-		'url': build_url(title),
-		'jsonpCallback': 'scrape'
+		'dataType': 'xml',
+		'url': 'server/wp.php',
+		'success': scrape,
+		'data': {
+			'url': build_url(title)
+		}
 	});
 }
 
 function scrape(article) {
-	console.log(article.query.export['*']);
+	console.log(article);
 }
-get_html('Andrew');
+get_html('To Kill a Mockingbird');
+
+// function scraper() {
+// 	// do all the scraping
+// 	console.log($(body).text());
+// 	return $(body).text();
+// }
+
+// console.log("Adding scraper");
+// pjs.addScraper(build_url("To Kill a Mockingbird"), scraper);
+// console.log("Scraper added");
